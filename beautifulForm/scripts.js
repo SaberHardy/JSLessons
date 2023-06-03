770e53c17b38b12727e330c4b62382b7e44d1464
SaberHardy <samirsamado5@gmail.com>
2023-06-03 13:44:12 -0500

    validate next password slide

--- beautifulForm/scripts.js
+++ beautifulForm/scripts.js
@@ -1,3 +1,32 @@
+function animatedForm() {
+    const arrows = document.querySelectorAll('.fa-arrow-circle-right');
+
+    arrows.forEach(arrow => {
+
+        arrow.addEventListener('click', () => {
+            // this will return to us the current element
+            // the input that the user see
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            const nextForm = parent.nextElementSibling;
+
+            if (input.type === 'text' && validateUser(input)) {
+                nextSlide(parent, nextForm);
+            } else if (input.type === 'email' && validateEmail(input)) {
+                nextSlide(parent, nextForm);
+            } else if (input.type === 'password' && validatePassword(input)) {
+                nextSlide(parent, nextForm);
+            } else {
+                parent.style.animation = "shake 0.5s ease";
+            }
+            // get the animation again
+            parent.addEventListener('animationend', () => {
+                parent.style.animation = "";
+            });
+        });
+    });
+}
+
 function nextSlide(parent, nextForm) {
     parent.classList.add('inactive');
     parent.classList.remove('active');
@@ -17,35 +46,21 @@ function validateEmail(email) {
     const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (validation.test(email.value)) {
         error('linear-gradient(#94f199, #4e7efc)');
-
         return true;
     } else {
         error('#FD3E52FF');
     }
 }
 
-function animatedForm() {
-    const arrows = document.querySelectorAll('.fa-arrow-circle-right');
-
-    arrows.forEach(arrow => {
-
-        arrow.addEventListener('click', () => {
-            // this will return to us the current element
-            // the input that the user see
-            const input = arrow.previousElementSibling;
-            const parent = arrow.parentElement;
-            const nextForm = parent.nextElementSibling;
-
-            if (input.type === 'text' && validateUser(input)) {
-                nextSlide(parent, nextForm);
-            } else if (input.type === 'email' && validateEmail(input)) {
-                nextSlide(parent, nextForm);
-
-            }
-        });
-    });
-}
-
+const validatePassword = (password) => {
+    const validation = /^[a-zA-Z0-9]{6,18}$/;
+    if (validation.test(password.value)) {
+        error('linear-gradient(#94f199, #4e7efc)');
+        return true;
+    } else {
+        error('#FD3E52FF');
+    }
+};
 
 function error(color) {
     document.body.style.background = color;
