ce9e3f5fa482775927c91734c889fa8e8b43d0aa
SaberHardy <samirsamado5@gmail.com>
2023-06-09 16:46:50 -0500

    add post to div in js

--- TodoApp/main.js
+++ TodoApp/main.js
@@ -1,7 +1,8 @@
 let form = document.getElementById('form');
 let input = document.getElementById('input');
 let msg = document.getElementById('msg');
-let post = document.getElementById('posts');
+let posts = document.getElementById('posts');
+let data = {};
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -11,10 +12,29 @@ form.addEventListener('submit', (event) => {
 let formValidation = () => {
     if (input.value === '') {
         msg.innerHTML = "One of your fields is empty!!";
-
     } else {
         console.log('form submitted');
+        acceptData();
+
         msg.innerHTML = "";
-        input.value = '';
     }
 };
+
+let acceptData = () => {
+    data['text'] = input.value;
+    console.log(data);
+    createPost();
+}
+
+let createPost = () => {
+    posts.innerHTML += `
+    <div>
+        <p>${data.text}</p>
+        <span class="options">
+            <i onclick="editPost(this)" class="fas fa-edit"></i>
+            <i onclick="deletPost(this)" class="fas fa-trash-alt"></i>
+        </span>
+    </div>
+    `;
+    input.value = '';
+}
