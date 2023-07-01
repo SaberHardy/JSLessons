0160551b0a8e38c6f3034465011a2ffad81631e3
SaberHardy <samirsamado5@gmail.com>
2023-07-01 15:29:07 -0500

    style the questions

---
+++ MultiProjs/quizApp/quizApp.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+        question: 'What is my name?',
+        a: '10',
+        b: '20',
+        c: '30',
+        d: '45',
+        correct: 'b',
+    },
+    {
+        question: 'How many days we have?',
+        a: '77',
+        b: '7',
+        c: '1',
+        d: '5',
+        correct: 'b',
+    },
+    {
+        question: 'What is the best programing language?',
+        a: 'Python',
+        b: 'Js',
+        c: 'Java',
+        d: 'Matlab',
+        correct: 'a',
+    },
+    {
+        question: 'What is the best place to live?',
+        a: 'Algeria',
+        b: 'USA',
+        c: 'Kube',
+        d: 'Brazil',
+        correct: 'a',
+    },
+]
